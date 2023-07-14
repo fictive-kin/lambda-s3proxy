1e33d0f5aa1d32005846715da5545be9363a4274
Jared McKnight <jmcknight@archertech.ca>
2023-07-14 15:17:14 -0400

    adds ability to use pre-built json file from S3 bucket

--- application/geography.py
+++ application/geography.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import json
 from urllib.parse import unquote
 
-from flask import Flask, Blueprint, request, jsonify
+import botocore
+from flask import Flask, Blueprint, abort, request, jsonify
 from flask_cors import cross_origin
 
 try:
@@ -36,6 +38,20 @@ AS_FLOAT = [
 
 REGION_UNIT = {
     'US': Unit.MILES,
+    'UK': Unit.MILES,
+}
+
+POINT_NEMO_HEADERS = {
+    'country_code': 'N/A',
+    'city': 'Point Nemo',
+    'country_name': 'N/A',
+    'region_code': 'N/A',
+    'region_name': 'Pacific Ocean',
+    'latitude': -48.8767,
+    'longitude': -123.3933,
+    'metro-code': 'N/A',
+    'postal_code': 'N/A',
+    'timezone': 'Etc/GMT-9',
 }
 
 
@@ -68,7 +84,7 @@ class FlaskGeography:
                         return_value = value
 
                     if header.lower() in AS_FLOAT:
-                        value = float(value)
+                        return_value = float(return_value)
 
                     returnable.update({
                         DESIRED_HEADERS[header.lower()]: return_value
@@ -77,18 +93,7 @@ class FlaskGeography:
             if not returnable:
                 # When we're not running behind CloudFront, we want valid data,
                 # but want to be able to tell easily that it's inaccurate.
-                returnable = {
-                    'country_code': 'N/A',
-                    'city': 'Point Nemo',
-                    'country_name': 'N/A',
-                    'region_code': 'N/A',
-                    'region_name': 'Pacific Ocean',
-                    'latitude': -48.8767,
-                    'longitude': -123.3933,
-                    'metro-code': 'N/A',
-                    'postal_code': 'N/A',
-                    'timezone': 'Etc/GMT-9',
-                }
+                returnable = POINT_NEMO_HEADERS
 
             return returnable
 
@@ -97,13 +102,68 @@ class FlaskGeography:
         def geography():
             return jsonify(cf_to_normal(request.headers))
 
-        @app.route(f'{route}/distances', methods=['POST'])
+        @app.route(f'{route}/closest-to-user/<path:filename>', methods=['GET'])
+        def closest_to_user(filename):
+            if not app.s3_proxy:
+                return abort(404)
+
+            try:
+                data_file = app.s3_proxy.get_file(f'{filename}.json')
+
+            except botocore.exceptions.ClientError as exc:
+                if exc.response['Error']['Code'] == 'NoSuchKey':
+                    app.logger.warning(f"/{filename}.json does not exist!")
+                    return abort(404)
+                else:
+                    raise
+
+            try:
+                data = {
+                    'points': json.load(data_file['Body']),
+                }
+
+            except json.JSONDecodeError as exc:
+                app.logger.error(f'/{filename}.json is not valid json')
+                app.logger.exception(exc)
+                return abort(404)
+
+            if request.args.get('unit'):
+                data.update({'unit': request.args['unit']})
+
+            return get_distances(data)
+
+        @app.route(f'{route}/closest-to-user', methods=['POST'])
         def distances():
+            return get_distances(request.json)
+
+        def get_distances(data):
+            """
+            data is a dict of:
+
+            {
+                'unit': Unit.KILOMETERS,  # Optional, can be any of the supported units of Haversine
+                'home': [  # Optional latitude/longitude pair. default is taken from CF headers
+                    45.1234,
+                    -53.1234,
+                ],
+                'points': {
+                   # Required, list of points to compare against, a unique id + lat/long pair + any desired extra keys
+                   'id1': {
+                       'latitude': 45.1235,
+                       'longitude': -53.1234,
+                       ...other ignored keys
+                   },
+                   ...
+                }
+            }
+            """
+
             if not HAS_HAVERSINE:
                 return abort(404)
 
             localization = cf_to_normal(request.headers)
-            data = request.json
+            if localization == POINT_NEMO_HEADERS:
+                return abort(404)
 
             if not data.get('points'):
                 raise abort(400)
@@ -119,15 +179,35 @@ class FlaskGeography:
             closest = None
             points = {}
 
-            for id_,ll in data['points'].items():
-                distance = haversine(home, ll, unit=unit)
-                if closest is None or distance < closest['distance']:
-                    closest = {
-                        'id': id_,
-                        'distance': distance,
-                    }
+            if isinstance(data['points'], (list, set,)):
+                for point in data['points']:
+                    if 'id' not in point or 'latitude' not in point or 'longitude' not in point:
+                        continue
 
-                points.update({id_: distance})
+                    id_ = point['id']
+                    ll = (float(point['latitude']), float(point['longitude']),)
+
+                    distance = haversine(home, ll, unit=unit)
+                    if closest is None or distance < closest['distance']:
+                        closest = {
+                            'id': id_,
+                            'distance': distance,
+                        }
+
+                    points.update({id_: point})
+                    points[id_].update({'distance': distance})
+
+            else:
+                for id_,ll in data['points'].items():
+                    distance = haversine(home, ll, unit=unit)
+                    if closest is None or distance < closest['distance']:
+                        closest = {
+                            'id': id_,
+                            'distance': distance,
+                        }
+
+                    points.update({id_: point})
+                    points[id_].update({'distance': distance})
 
             return jsonify({
                 'unit': unit,
